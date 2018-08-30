7c106a6a074ef3679789311b77407f52ea4620ab
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-30 10:02:40 +0000

    12c69b1 Prevent text selection on checkbox label

--- Input/Checkbox.js
+++ Input/Checkbox.js
@@ -17,12 +17,29 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var bem = (0, _utils.bemClassNames)('d2ui-checkbox');
 
-var Checkbox = function Checkbox(props) {
+var Checkbox = function Checkbox(_ref) {
+  var children = _ref.children;
   return _react.default.createElement("label", {
+    className: bem.e('label-wrap')
+  }, _react.default.createElement("div", {
     className: bem.b()
   }, _react.default.createElement("input", {
-    type: "checkbox"
-  }), _react.default.createElement("span", null, "Checkbox"));
+    type: "checkbox",
+    className: bem.e('native-control')
+  }), _react.default.createElement("div", {
+    className: bem.e('background')
+  }, _react.default.createElement("svg", {
+    className: bem.e('checkmark'),
+    viewBox: "0 0 24 24"
+  }, _react.default.createElement("path", {
+    className: bem.e('checkmark-path'),
+    fill: "none",
+    d: "M1.73,12.91 8.1,19.28 22.79,4.59"
+  })), _react.default.createElement("div", {
+    className: bem.e('mixedmark')
+  }))), _react.default.createElement("span", {
+    className: bem.e('label-text')
+  }, children));
 };
 
 var _default = Checkbox;
