6aa8b45a471adb15c0d42c92bbe5cf55926e6fce
GitHub <noreply@github.com>
2018-09-05 12:10:22 +0000

    de00082 Merge pull request #20 from dhis2/dir-and-theme-helper

--- utils/index.js
+++ utils/index.js
@@ -23,11 +23,19 @@ Object.defineProperty(exports, "wrapTextNodesInSpans", {
     return _wrapTextNodesInSpans.default;
   }
 });
+Object.defineProperty(exports, "setTextDirection", {
+  enumerable: true,
+  get: function get() {
+    return _setTextDirection.default;
+  }
+});
 
 var _isRtl = _interopRequireDefault(require("./isRtl"));
 
 var _bemClassNames = _interopRequireDefault(require("./bemClassNames"));
 
 var _wrapTextNodesInSpans = _interopRequireDefault(require("./wrapTextNodesInSpans"));
+
+var _setTextDirection = _interopRequireDefault(require("./setTextDirection"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
