91fc65781994423bd052ec5d9e2fe1bbf505d8b8
Muhammad Adeel <imadeelshahid@gmail.com>
2018-09-21 16:05:50 +0000

    bba3c16 integrate code-style into dhis2/ui

---
+++ HeaderBar/Profile.js
@@ -0,0 +1,42 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Apps;
+
+require("core-js/modules/es6.regexp.split");
+
+require("core-js/modules/es6.array.index-of");
+
+require("core-js/modules/es6.function.name");
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** @format */
+function TextIcon(_ref) {
+  var name = _ref.name;
+  var title = name[0];
+
+  if (name.indexOf(' ') > 0) {
+    title += name.split(' ')[1][0];
+  }
+
+  return _react.default.createElement("div", {
+    className: "icon"
+  }, _react.default.createElement("div", {
+    className: "initials"
+  }, title));
+}
+
+function Apps() {
+  return _react.default.createElement("div", {
+    className: "profile"
+  }, _react.default.createElement(TextIcon, {
+    name: "Adam Adam"
+  }));
+}
---
+++ HeaderBar/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _HeaderBar = _interopRequireDefault(require("./HeaderBar"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** @format */
+var _default = _HeaderBar.default;
+exports.default = _default;
