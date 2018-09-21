abcfdb7851aff9b8bc006965c6e8178c291dbf5a
Viktor Varland <viktor@vardevs.se>
2018-09-21 16:13:57 +0000

    eee61a8 chore: cut new beta release

---
+++ utils/easings.js
@@ -0,0 +1,18 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+// Adapted from https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
+var easeInOutQuad = function easeInOutQuad(currentTime, initialValue, change, duration) {
+  return (currentTime /= duration / 2) < 1 ? change / 2 * currentTime * currentTime + initialValue : -change / 2 * (--currentTime * (currentTime - 2) - 1) + initialValue;
+};
+
+var _default = {
+  easeInOutQuad: easeInOutQuad
+};
+exports.default = _default;
