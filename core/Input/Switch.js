67f346d73da9b325b53ec1547151143c70c6bcca
Viktor Varland <viktor@vardevs.se>
2018-09-27 11:21:08 +0000

    18a8654 refactor(Paper): use className to allow overriding styles

---
+++ core/Input/Switch.js
@@ -0,0 +1,81 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.Switch = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _ToggleField = _interopRequireDefault(require("./ToggleField"));
+
+require("./switch.css");
+
+var _utils = require("../../utils");
+
+var _FieldWrap = _interopRequireDefault(require("./FieldWrap"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** @format */
+var bem = (0, _utils.bemClassNames)('switch');
+
+var Switch = function Switch(_ref) {
+  var label = _ref.label,
+      checked = _ref.checked,
+      valid = _ref.valid,
+      warning = _ref.warning,
+      error = _ref.error,
+      disabled = _ref.disabled,
+      onChange = _ref.onChange,
+      dense = _ref.dense,
+      block = _ref.block,
+      helpText = _ref.helpText,
+      required = _ref.required;
+  return _react.default.createElement(_FieldWrap.default, {
+    valid: valid,
+    warning: warning,
+    disabled: disabled,
+    error: error,
+    dense: dense,
+    block: block,
+    helpText: helpText
+  }, _react.default.createElement(_ToggleField.default, {
+    disabled: disabled,
+    dense: dense
+  }, _react.default.createElement("div", {
+    className: bem.b({
+      checked: checked,
+      disabled: disabled
+    })
+  }, _react.default.createElement("div", {
+    className: bem.e('track')
+  }), _react.default.createElement("div", {
+    className: bem.e('thumb-underlay')
+  }, _react.default.createElement("div", {
+    className: bem.e('thumb')
+  }, _react.default.createElement("input", {
+    onChange: onChange,
+    type: "checkbox",
+    id: "basic-switch",
+    className: bem.e('native-control'),
+    "aria-checked": checked,
+    checked: checked,
+    role: "switch",
+    disabled: disabled
+  })))), _react.default.createElement("span", {
+    className: bem.e('label-text', {
+      disabled: disabled
+    })
+  }, (0, _utils.getRequiredText)(label, required))));
+};
+
+exports.Switch = Switch;
+Switch.defaultProps = {
+  checked: false,
+  disabled: false
+};
+var _default = Switch;
+exports.default = _default;
---
+++ core/Paper/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Paper = Paper;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _utils = require("../../utils");
+
+require("./styles.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** @format */
+var bem = (0, _utils.bemClassNames)('paper');
+
+function Paper(_ref) {
+  var elevation = _ref.elevation,
+      children = _ref.children,
+      className = _ref.className;
+  return _react.default.createElement("div", {
+    className: bem.b("elevation-".concat(elevation, " ").concat(className))
+  }, children);
+}
+
+Paper.defaultProps = {
+  elevation: 1,
+  className: 'padded'
+};
+var _default = Paper;
+exports.default = _default;
