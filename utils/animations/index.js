fcd69d95c38cf5d5ce94f5e51713f8c0b476b966
Muhammad Adeel <adeel@dhis2.org>
2018-10-02 08:38:46 +0000

    b5c8c92 remove un-used file, defer func. already in utils/timings.js

---
+++ utils/animations/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+require("core-js/modules/es6.array.iterator");
+
+require("core-js/modules/es6.object.keys");
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.for-each");
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _general = require("./general");
+
+Object.keys(_general).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _general[key];
+    }
+  });
+});
+
+var _easing = require("./easing");
+
+Object.keys(_easing).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _easing[key];
+    }
+  });
+});
+
+var _AnimatedCloseHOC = require("./AnimatedCloseHOC");
+
+Object.keys(_AnimatedCloseHOC).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _AnimatedCloseHOC[key];
+    }
+  });
+});
