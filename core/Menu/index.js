22e7c20f53746f897110b3c57b47b4c1c912e71d
Muhammad Adeel <adeel@dhis2.org>
2018-10-02 09:35:47 +0000

    dbf8773 document functions

---
+++ core/Menu/index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+require("core-js/modules/es6.array.iterator");
+
+require("core-js/modules/es6.object.keys");
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.for-each");
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _DropdownMenu = require("./DropdownMenu");
+
+Object.keys(_DropdownMenu).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _DropdownMenu[key];
+    }
+  });
+});
+
+var _Menu = require("./Menu");
+
+Object.keys(_Menu).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Menu[key];
+    }
+  });
+});
+
+var _MenuItem = require("./MenuItem");
+
+Object.keys(_MenuItem).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _MenuItem[key];
+    }
+  });
+});
+
+var _PopoverMenu = require("./PopoverMenu");
+
+Object.keys(_PopoverMenu).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _PopoverMenu[key];
+    }
+  });
+});
