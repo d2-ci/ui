602fd9070c41852a9da7028ef904c166089daaed
GitHub <noreply@github.com>
2018-10-02 09:07:16 +0000

    39d061d refactor(RTL) (#52)

--- utils/rtl.js
+++ utils/rtl.js
@@ -5,37 +5,34 @@ require("core-js/modules/es6.object.define-property");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.setTextDirection = setTextDirection;
-exports.isRTL = isRTL;
-exports.RTL_LANGUAGES = void 0;
+exports.setDir = setDir;
+exports.isDocRTL = isDocRTL;
+exports.RTL_LANGS = void 0;
+
+require("core-js/modules/es6.regexp.split");
 
 require("core-js/modules/es7.array.includes");
 
 require("core-js/modules/es6.string.includes");
 
-require("core-js/modules/es6.regexp.split");
+var _html = require("./html");
 
+/** @format */
+// https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
+var RTL_LANGS = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi'];
 /**
- * This function will set the dir attribute on the HTML tag based.
- * Since it takes a userSettings object as an option and returns them too, it fits nicely into a chained promise sequence used to bootstrap a DHIS2 React app:
  *
- * @format
- * @example ... .then(getUserSettings) .then(setTextDirection) .then(configI18n) ...
- * @example
- * @param {Object} userSettings - The userSettings available in on the d2 object (d2.currentUser.userSettings.settings) or returned from the from the `getUserSettings` function exposed by d2
+ * @param locale format "en" or "en_EN"
  */
-function setTextDirection(userSettings) {
-  // Will produce "en" from "en_EN" as well as "en"
-  var uiLanguage = userSettings.keyUiLocale.split('_')[0];
-  var dir = RTL_LANGUAGES.includes(uiLanguage) ? 'rtl' : 'ltr';
-  document.documentElement.setAttribute('dir', dir);
-  return userSettings;
-} // https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
 
+exports.RTL_LANGS = RTL_LANGS;
 
-var RTL_LANGUAGES = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi'];
-exports.RTL_LANGUAGES = RTL_LANGUAGES;
+function setDir(locale) {
+  var uiLanguage = locale.includes('_') ? locale.split('_')[0] : locale;
+  var dir = RTL_LANGS.includes(uiLanguage) ? 'rtl' : 'ltr';
+  (0, _html.getDoc)().setAttribute('dir', dir);
+}
 
-function isRTL() {
-  return document.documentElement.getAttribute('dir') === 'rtl';
+function isDocRTL() {
+  return (0, _html.getDoc)().getAttribute('dir') === 'rtl';
 }
