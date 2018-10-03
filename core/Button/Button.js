130df78740716bb7b059f9435de33b3752589a47
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-03 13:11:17 +0000

    cd2b9d8 Button

---
+++ core/Button/Button.js
@@ -0,0 +1,52 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Button = Button;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _utils = require("../../utils");
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** @format */
+function Button(_ref) {
+  var role = _ref.role,
+      type = _ref.type,
+      size = _ref.size,
+      kind = _ref.kind,
+      title = _ref.title,
+      disabled = _ref.disabled,
+      onClick = _ref.onClick,
+      onDoubleClick = _ref.onDoubleClick,
+      children = _ref.children;
+  return _react.default.createElement("button", {
+    role: role,
+    type: type,
+    title: title,
+    disabled: disabled,
+    onClick: onClick,
+    onDoubleClick: onDoubleClick,
+    className: (0, _styles.default)('button', 'align-icon', kind, size)
+  }, (0, _utils.wrapTextNodesInSpans)(children));
+}
+
+Button.defaultProps = {
+  role: 'button',
+  type: 'button',
+  size: 'medium',
+  kind: 'raised',
+  title: '',
+  disabled: false,
+  onClick: undefined,
+  onDoubleClick: undefined
+};
+var _default = Button;
+exports.default = _default;
---
+++ utils/rtl.js
@@ -0,0 +1,49 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.setDocDir = setDocDir;
+exports.isDocRTL = isDocRTL;
+exports.RTL_LANGS = void 0;
+
+require("core-js/modules/es6.regexp.split");
+
+require("core-js/modules/es7.array.includes");
+
+require("core-js/modules/es6.string.includes");
+
+var _html = require("./html");
+
+/** @format */
+
+/**
+ * prefixes for Right to Left (RTL) languages
+ *
+ * @type {string[]}
+ * @link https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
+ */
+var RTL_LANGS = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi'];
+/**
+ * set dir attribute {ltr, rtl} on document based on locale
+ * @param locale format "en" or "en_EN"
+ */
+
+exports.RTL_LANGS = RTL_LANGS;
+
+function setDocDir(locale) {
+  var uiLanguage = locale.includes('_') ? locale.split('_')[0] : locale;
+  var dir = RTL_LANGS.includes(uiLanguage) ? 'rtl' : 'ltr';
+  (0, _html.getDoc)().setAttribute('dir', dir);
+}
+/**
+ * returns true, if document is rendered in RTL mode
+ * @returns {boolean}
+ */
+
+
+function isDocRTL() {
+  return (0, _html.getDoc)().getAttribute('dir') === 'rtl';
+}
