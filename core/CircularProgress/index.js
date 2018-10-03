1836739ce49ba4c6d4410e84016cea7aeec246d2
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-03 14:51:42 +0000

    a483b77 rename

--- core/CircularProgress/index.js
+++ core/CircularProgress/index.js
@@ -15,7 +15,7 @@ var _styles = _interopRequireDefault(require("./styles"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /** @format */
-function Icon(_ref) {
+function Circle(_ref) {
   var dashed = _ref.dashed;
   return _react.default.createElement("svg", {
     viewBox: "22 22 44 44",
@@ -38,20 +38,20 @@ function CircularProgress(_ref2) {
       center = _ref2.center,
       dashed = _ref2.dashed;
 
-  var view = _react.default.createElement("div", {
+  var loader = _react.default.createElement("div", {
     role: "progressbar",
     className: (0, _styles.default)('container', size)
-  }, _react.default.createElement(Icon, {
+  }, _react.default.createElement(Circle, {
     dashed: dashed
   }));
 
   if (center) {
     return _react.default.createElement("div", {
       className: (0, _styles.default)('overlay')
-    }, view);
+    }, loader);
   }
 
-  return view;
+  return loader;
 }
 
 CircularProgress.defaultProps = {
