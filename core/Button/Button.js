73febc81ad2eae42057a2a132143f2bfb2c734cb
GitHub <noreply@github.com>
2018-10-03 13:39:01 +0000

    1259e5a Merge pull request #60 from dhis2/refactor-button

--- core/Button/Button.js
+++ core/Button/Button.js
@@ -5,47 +5,46 @@ require("core-js/modules/es6.object.define-property");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.Button = void 0;
-
-require("core-js/modules/es6.object.assign");
+exports.Button = Button;
+exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
-
-var _utils = require("../../utils");
 
 var _styles = _interopRequireDefault(require("./styles"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-var Button = function Button(_ref) {
-  var children = _ref.children,
+/** @format */
+function Button(_ref) {
+  var role = _ref.role,
+      type = _ref.type,
+      size = _ref.size,
+      kind = _ref.kind,
+      title = _ref.title,
       disabled = _ref.disabled,
       onClick = _ref.onClick,
       onDoubleClick = _ref.onDoubleClick,
-      role = _ref.role,
-      size = _ref.size,
-      title = _ref.title,
-      type = _ref.type,
-      kind = _ref.kind;
-  return _react.default.createElement("button", _extends({
-    className: (0, _styles.default)('button', 'align-icon', kind, size)
-  }, {
+      children = _ref.children;
+  return _react.default.createElement("button", {
+    role: role,
+    type: type,
+    title: title,
     disabled: disabled,
     onClick: onClick,
     onDoubleClick: onDoubleClick,
-    title: title,
-    type: type,
-    role: role
-  }), (0, _utils.wrapTextNodesInSpans)(children));
-};
+    className: (0, _styles.default)('button', 'align-icon', kind, size)
+  }, children);
+}
 
-exports.Button = Button;
 Button.defaultProps = {
+  role: 'button',
   type: 'button',
   size: 'medium',
-  kind: 'raised'
+  kind: 'raised',
+  title: '',
+  disabled: false,
+  onClick: undefined,
+  onDoubleClick: undefined
 };
 var _default = Button;
 exports.default = _default;
--- core/Tabs/computeHorizontalScrollbarHeight.js
+++ core/Tabs/computeHorizontalScrollbarHeight.js
@@ -14,9 +14,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 /** @format */
 var horizontalScrollbarHeight;
 /**
- * @link https://github.com/material-components/material-components-web/blob/master/packages/mdc-tab-scroller/util.js#L39-L55
  * Computes the height of browser-rendered horizontal scrollbars using a self-created test element.
  * May return 0 (e.g. on OS X browsers under default configuration).
+ *
+ * @link https://github.com/material-components/material-components-web/blob/master/packages/mdc-tab-scroller/util.js#L39-L55
+ *
  * @param {!Document} documentObj
  * @param {boolean=} shouldCacheResult
  * @return {number}
