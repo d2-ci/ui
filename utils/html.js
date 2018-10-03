946cb65adc4832de9125d096964c748b009d2c2c
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-03 10:38:06 +0000

    114079a style(css): Separate base styles from theme files

---
+++ utils/html.js
@@ -0,0 +1,52 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.wrapTextNodesInSpans = wrapTextNodesInSpans;
+exports.getDoc = getDoc;
+
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es6.symbol");
+
+require("core-js/modules/es7.array.includes");
+
+require("core-js/modules/es6.string.includes");
+
+require("core-js/modules/es6.array.map");
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+/**
+ * CSS selectors like :first-child, :only-child, :last-child do not take text nodes
+ * into account, so wrapping text nodes in spans simplifies the CSS file a lot.
+ *
+ * @param nodes list of text nodes
+ */
+function wrapTextNodesInSpans(nodes) {
+  return _react.default.Children.map(nodes, function (node, idx) {
+    if (['string', 'number'].includes(_typeof(node))) {
+      return _react.default.createElement("span", {
+        key: "key-".concat(idx)
+      }, node);
+    }
+
+    return node;
+  });
+}
+/**
+ * get <html> or the <body> element in document
+ * @returns {HTMLElement}
+ */
+
+
+function getDoc() {
+  return document.documentElement || document.body;
+}
---
+++ utils/index.js
@@ -0,0 +1,99 @@
+"use strict";
+
+require("core-js/modules/es6.array.iterator");
+
+require("core-js/modules/es6.object.keys");
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.for-each");
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _html = require("./html");
+
+Object.keys(_html).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _html[key];
+    }
+  });
+});
+
+var _css = require("./css");
+
+Object.keys(_css).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _css[key];
+    }
+  });
+});
+
+var _rtl = require("./rtl");
+
+Object.keys(_rtl).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _rtl[key];
+    }
+  });
+});
+
+var _text = require("./text");
+
+Object.keys(_text).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _text[key];
+    }
+  });
+});
+
+var _timings = require("./timings");
+
+Object.keys(_timings).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _timings[key];
+    }
+  });
+});
+
+var _api = require("./api");
+
+Object.keys(_api).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _api[key];
+    }
+  });
+});
+
+var _index = require("./animations/index");
+
+Object.keys(_index).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _index[key];
+    }
+  });
+});
