3ab61f833b66b7e8a63e8ef144f58e51dfa6ded0
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-04 07:02:37 +0000

    e494497 refactor(Dialog)

---
+++ core/Icon/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Icon = Icon;
+exports.default = void 0;
+
+require("core-js/modules/es6.object.define-property");
+
+require("core-js/modules/es6.function.name");
+
+var _react = _interopRequireDefault(require("react"));
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function Icon(_ref) {
+  var name = _ref.name,
+      className = _ref.className,
+      onClick = _ref.onClick;
+  return _react.default.createElement("i", {
+    onClick: onClick,
+    className: (0, _styles.default)('container', _defineProperty({}, className, !!className))
+  }, name);
+}
+
+Icon.defaultProps = {
+  name: '',
+  className: '',
+  onClick: undefined
+};
+var _default = Icon;
+exports.default = _default;
