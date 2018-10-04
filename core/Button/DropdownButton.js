10f25e65bd2d0d787fc2c98aed520b159bd5bc76
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-04 14:53:37 +0000

    81946f8 refactor(DropdownButton)

---
+++ core/Button/DropdownButton.js
@@ -0,0 +1,163 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.DropdownButton = void 0;
+
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es6.symbol");
+
+require("core-js/modules/es6.object.define-property");
+
+require("core-js/modules/es6.object.create");
+
+require("core-js/modules/es6.object.set-prototype-of");
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _Button = _interopRequireDefault(require("./Button"));
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+var _Icon = _interopRequireDefault(require("../Icon"));
+
+var _Menu = _interopRequireDefault(require("../Menu/Menu"));
+
+var _utils = require("../../utils");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+/**
+ * list of menu items opens up on clicking KeyDown icon on the right side
+ */
+var DropdownButton =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(DropdownButton, _Component);
+
+  function DropdownButton() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, DropdownButton);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(DropdownButton)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
+      open: false
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "onDocClick", function (evt) {
+      if (_this.elContainer && _this.elMenu) {
+        var target = {
+          x: evt.clientX,
+          y: evt.clientY
+        };
+
+        var apps = _this.elMenu.getBoundingClientRect();
+
+        var container = _this.elContainer.getBoundingClientRect();
+
+        if (!(0, _utils.isPointInRect)(target, apps) && !(0, _utils.isPointInRect)(target, container)) {
+          _this.setState({
+            open: false
+          });
+        }
+      }
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "onToggle", function () {
+      return _this.setState({
+        open: !_this.state.open
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "onClose", function () {
+      return _this.setState({
+        open: false
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(DropdownButton, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      document.addEventListener('click', this.onDocClick);
+    }
+  }, {
+    key: "componentWillMount",
+    value: function componentWillMount() {
+      document.removeEventListener('click', this.onDocClick);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var open = this.state.open;
+      return _react.default.createElement("div", {
+        className: (0, _styles.default)('dropdown'),
+        ref: function ref(c) {
+          return _this2.elContainer = c;
+        }
+      }, _react.default.createElement(_Button.default, {
+        kind: this.props.kind,
+        onClick: this.props.onClick
+      }, this.props.label), _react.default.createElement(_Button.default, {
+        kind: this.props.kind,
+        onClick: this.onToggle
+      }, _react.default.createElement(_Icon.default, {
+        name: open ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+      })), open && _react.default.createElement("div", {
+        className: (0, _styles.default)('menu'),
+        ref: function ref(c) {
+          return _this2.elMenu = c;
+        }
+      }, _react.default.createElement(_Menu.default, {
+        list: this.props.list,
+        onClose: this.onClose,
+        onSelect: this.props.onSelect
+      })));
+    }
+  }]);
+
+  return DropdownButton;
+}(_react.Component);
+
+exports.DropdownButton = DropdownButton;
+DropdownButton.defaultProps = {
+  kind: 'primary'
+};
+var _default = DropdownButton;
+exports.default = _default;
---
+++ utils/theme.js
@@ -0,0 +1,32 @@
+"use strict";
+
+require("core-js/modules/es6.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.t = t;
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.for-each");
+
+/**
+ * Provide static theme classes for components to be used in cx.bind
+ * e.g. 'primary', 'primary-bg'
+ * @returns { 'primary': true , 'primary-bg': true }
+ */
+function t() {
+  var obj = {};
+
+  for (var _len = arguments.length, classes = new Array(_len), _key = 0; _key < _len; _key++) {
+    classes[_key] = arguments[_key];
+  }
+
+  classes.forEach(function (n) {
+    if (typeof n === 'string' || typeof n === 'number' || !!n) {
+      obj[n] = true;
+    }
+  });
+  return obj;
+}
