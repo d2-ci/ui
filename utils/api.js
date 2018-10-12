a793853e282d8236dfba48ec5ee8467e64c1c4b4
Viktor Varland <viktor@vardevs.se>
2018-10-12 11:54:02 +0000

    54944e2 docs: docs before format

--- utils/api.js
+++ utils/api.js
@@ -18,27 +18,30 @@ require("core-js/modules/es6.object.assign");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 /* global DHIS_CONFIG, manifest */
-var version = '30'; // let url = 'http://localhost:8080'
-
-var url = 'https://play.dhis2.org/2.30';
-
-if (typeof manifest !== 'undefined') {
-  version = manifest.dhis2.apiVersion;
-}
-
+var version;
+var url;
+var endpoint;
 var isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
-  url = manifest.activities.dhis.href;
-} else if (!isProd && (typeof DHIS_CONFIG === "undefined" ? "undefined" : _typeof(DHIS_CONFIG)) === 'object') {
-  url = DHIS_CONFIG.baseUrl;
-} else if (isProd) {
-  url = '..';
+  if (manifest) {
+    url = manifest.activities.dhis.href;
+    version = manifest.dhis2.apiVersion;
+    endpoint = "".concat(serverURL, "/api/").concat(version);
+  } else if ((typeof DHIS_CONFIG === "undefined" ? "undefined" : _typeof(DHIS_CONFIG)) === 'object') {
+    url = DHIS_CONFIG.baseUrl;
+  } else {
+    url = '..';
+  }
+
+  if (!endpoint) {
+    endpoint = "".concat(url, "/api");
+  }
 }
 
 var serverURL = url;
 exports.serverURL = serverURL;
-var apiEndpoint = "".concat(serverURL, "/api/").concat(version);
+var apiEndpoint = endpoint;
 exports.apiEndpoint = apiEndpoint;
 
 function getPath() {
