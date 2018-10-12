f4ccf5ca002671a6778f0bf61fd94f8c58958d2f
Viktor Varland <viktor@vardevs.se>
2018-10-12 11:50:37 +0000

    adfbaa0 docs: docs before format

---
+++ core/helpers/Divider/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Divider = Divider;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Divider(_ref) {
+  var margin = _ref.margin;
+  var style = {
+    margin: margin
+  };
+  return _react.default.createElement("div", {
+    style: style,
+    className: (0, _styles.default)('divider')
+  });
+}
+
+Divider.defaultProps = {
+  margin: '8px 0'
+};
+var _default = Divider;
+exports.default = _default;
