84787ac1c86d62995d82ab30ae4c4812e0095592
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-19 12:22:00 +0000

    c8be4d7 fix(headerbar-container)

---
+++ core/Input/shared/Field/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Field = Field;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Field(_ref) {
+  var children = _ref.children,
+      valid = _ref.valid,
+      warning = _ref.warning,
+      disabled = _ref.disabled,
+      error = _ref.error,
+      dense = _ref.dense,
+      block = _ref.block,
+      helpText = _ref.helpText,
+      className = _ref.className;
+  return _react.default.createElement("div", {
+    className: (0, _styles.default)('container', className, {
+      valid: valid,
+      warning: warning,
+      disabled: disabled,
+      error: error,
+      dense: dense,
+      'full-width': block
+    })
+  }, children, helpText && _react.default.createElement("div", {
+    className: (0, _styles.default)('help-text')
+  }, helpText));
+}
+
+Field.propTypes = {
+  // Because the Field is the root component for a number of inputs
+  // it is convenient to be able to add a custom className
+  className: _propTypes.default.string,
+  children: _propTypes.default.node.isRequired,
+  valid: _propTypes.default.bool,
+  warning: _propTypes.default.bool,
+  disabled: _propTypes.default.bool,
+  error: _propTypes.default.bool,
+  dense: _propTypes.default.bool,
+  block: _propTypes.default.bool,
+  helpText: _propTypes.default.string
+};
+var _default = Field;
+exports.default = _default;
---
+++ core/UI/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.UI = UI;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+require("typeface-roboto");
+
+require("material-design-icons/iconfont/material-icons.css");
+
+require("./base/index.css");
+
+require("./theme/index.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import as part of the UI component to make sure that the font is
+// loaded for selective imports which might not import the root index
+function UI(_ref) {
+  var theme = _ref.theme,
+      children = _ref.children;
+  return _react.default.createElement("div", {
+    className: theme
+  }, children);
+}
+
+UI.propTypes = {
+  theme: _propTypes.default.string,
+  children: _propTypes.default.node
+};
+UI.defaultProps = {
+  theme: 'blue'
+};
+var _default = UI;
+exports.default = _default;
