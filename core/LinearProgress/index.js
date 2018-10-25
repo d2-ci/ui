f3188a1afa2e7e31f8ca1f0438406bc52381a2a9
Viktor Varland <viktor@vardevs.se>
2018-10-25 10:07:56 +0000

    86db880 style(UI): set default fonts for LTR, RTL

---
+++ core/LinearProgress/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.LinearProgress = LinearProgress;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function LinearProgress(_ref) {
+  var amount = _ref.amount,
+      margin = _ref.margin;
+  var type = typeof amount === 'undefined' ? (0, _styles.default)('indeterminate') : (0, _styles.default)('determinate');
+  var style = amount ? {
+    width: "".concat(amount, "%")
+  } : null;
+  return _react.default.createElement("div", {
+    role: "progressbar",
+    className: (0, _styles.default)('container'),
+    style: {
+      margin: margin
+    }
+  }, _react.default.createElement("div", {
+    style: style,
+    className: (0, _styles.default)('progress', type)
+  }));
+}
+
+LinearProgress.propTypes = {
+  amount: _propTypes.default.number,
+  margin: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number])
+};
+var _default = LinearProgress;
+exports.default = _default;
