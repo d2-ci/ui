e1299087025944f9a25517373294b272f1649088
Viktor Varland <viktor@vardevs.se>
2018-11-20 11:58:41 +0000

    a73d252 chore: move mixins to subdir as postcss breaks on root css files

---
+++ core/UI/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+require("typeface-roboto/index.css");
+
+require("material-design-icons-iconfont/dist/material-design-icons.css");
+
+require("./base/index.css");
---
+++ widgets/HeaderBar/NotificationIcon.js
@@ -0,0 +1,45 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _Icon = _interopRequireDefault(require("../../core/Icon"));
+
+var _utils = require("../../utils");
+
+var _styles = _interopRequireDefault(require("./styles"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function NotificationIcon(_ref) {
+  var icon = _ref.icon,
+      count = _ref.count,
+      href = _ref.href;
+  return _react.default.createElement("a", {
+    className: (0, _styles.default)('notification', {
+      icon: icon
+    }),
+    href: href
+  }, count > 0 && _react.default.createElement("span", {
+    className: (0, _styles.default)('count')
+  }, count), _react.default.createElement(_Icon.default, {
+    name: icon
+  }));
+}
+
+NotificationIcon.defaultProps = {
+  count: 0
+};
+NotificationIcon.propTypes = {
+  icon: _propTypes.default.string.isRequired,
+  href: _propTypes.default.string.isRequired,
+  count: _propTypes.default.number
+};
+var _default = NotificationIcon;
+exports.default = _default;
