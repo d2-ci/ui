774a5736208468b3b83ac6b1f631b0d2634001c7
Viktor Varland <viktor@vardevs.se>
2018-12-19 18:54:36 +0000

    0899835 refactor: rx/cx for the helpers as well

--- core/helpers/Help/index.js
+++ core/helpers/Help/index.js
@@ -1,13 +1,13 @@
 import _defineProperty from "@babel/runtime-corejs2/helpers/esm/defineProperty";
 import React from 'react';
 import PropTypes from 'prop-types';
-import s from './styles';
+import { rx } from './styles';
 
 function Help(_ref) {
   var text = _ref.text,
       status = _ref.status;
   return React.createElement("p", {
-    className: s('reset', 'base', _defineProperty({}, "status-".concat(status), true))
+    className: rx('base', _defineProperty({}, "status-".concat(status), true))
   }, text);
 }
 
