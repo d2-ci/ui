e50d687e534f6a651610fc0a277776dd00dfa75a
Viktor Varland <viktor@vardevs.se>
2019-03-01 07:25:42 +0000

    3d109ff feat: add classname proptype to all components

---
+++ core/common.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _defaultExport = new String(".jsx-1459278500{box-sizing:border-box;font-family:Roboto,sans-serif;}");
+
+_defaultExport.__hash = "1459278500";
+var _default = _defaultExport;
+exports.default = _default;
