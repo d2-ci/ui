366c99e87420f92bfca05c51fd0eb0c13543a9e3
Viktor Varland <viktor@vardevs.se>
2019-03-05 06:34:31 +0000

    9a04960 chore(greenkeeper): stick react-scripts to v1

--- utils/index.js
+++ utils/index.js
@@ -1,33 +1,101 @@
 "use strict";
 
-require("core-js/modules/es6.object.define-property");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "isRtl", {
-  enumerable: true,
-  get: function get() {
-    return _isRtl.default;
-  }
-});
-Object.defineProperty(exports, "bemClassNames", {
-  enumerable: true,
-  get: function get() {
-    return _bemClassNames.default;
-  }
-});
-Object.defineProperty(exports, "wrapTextNodesInSpans", {
-  enumerable: true,
-  get: function get() {
-    return _wrapTextNodesInSpans.default;
-  }
+
+var _html = require("./html");
+
+Object.keys(_html).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _html[key];
+    }
+  });
 });
 
-var _isRtl = _interopRequireDefault(require("./isRtl"));
+var _url = require("./url");
 
-var _bemClassNames = _interopRequireDefault(require("./bemClassNames"));
+Object.keys(_url).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _url[key];
+    }
+  });
+});
 
-var _wrapTextNodesInSpans = _interopRequireDefault(require("./wrapTextNodesInSpans"));
+var _css = require("./css");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+Object.keys(_css).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _css[key];
+    }
+  });
+});
+
+var _rtl = require("./rtl");
+
+Object.keys(_rtl).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _rtl[key];
+    }
+  });
+});
+
+var _math = require("./math");
+
+Object.keys(_math).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _math[key];
+    }
+  });
+});
+
+var _timings = require("./timings");
+
+Object.keys(_timings).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _timings[key];
+    }
+  });
+});
+
+var _api = require("./api");
+
+Object.keys(_api).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _api[key];
+    }
+  });
+});
+
+var _index = require("./animations/index");
+
+Object.keys(_index).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _index[key];
+    }
+  });
+});
