de797bd5faf5fe8a088b58ad92d73f8133b8f6c7
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-12-15 14:34:42 +0000

    b8fe429 feat: add YEAR and ANY period types to PERIOD_TYPES constant

---
+++ build/cjs/httpfunctions/models/PeriodType.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PeriodType = void 0;
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+let PeriodType = exports.PeriodType = /*#__PURE__*/function (PeriodType) {
+  PeriodType["WEEK"] = "week";
+  PeriodType["MONTH"] = "month";
+  PeriodType["ANY"] = "any";
+  PeriodType["YEAR"] = "year";
+  return PeriodType;
+}({});
