db2e4c742f2f2e76e60eb9b148ae35b4a3726339
Jen Jones Arnesen <jennifer@dhis2.org>
2025-12-18 08:47:50 +0000

    52d624a Merge branch 'main' into eh/feat/clim-153-show-percentage-interval-confidence-in-tooltip

--- build/cjs/utils/Time.js
+++ build/cjs/utils/Time.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getPeriodNameFromId = exports.getPeriodISOFromId = void 0;
+exports.getPeriodNameFromId = void 0;
 const getPeriodNameFromId = periodId => {
   // this should be moved to utils probably
   // or actually just use import { getPeriodNameFromId } from '@dhis2/multi-period-dimension'
@@ -30,23 +30,3 @@ const getPeriodNameFromId = periodId => {
   return periodId;
 };
 exports.getPeriodNameFromId = getPeriodNameFromId;
-const getPeriodISOFromId = periodId => {
-  // this should be moved to utils probably
-  // or actually just use import { getPeriodNameFromId } from '@dhis2/multi-period-dimension'
-  // console.log('periodId', periodId)
-  if (!periodId) return 'NA';
-  if (periodId.length == 4) {
-    return periodId;
-  }
-  if (periodId.length == 6) {
-    const year = periodId.slice(0, 4);
-    const month = periodId.slice(4, 6);
-    return `${year}-${month}`;
-  }
-  if (periodId.length == 5) {
-    const year = periodId.slice(0, 4);
-    const week = periodId.slice(5, 6);
-    return `${year}-W${week}`;
-  }
-};
-exports.getPeriodISOFromId = getPeriodISOFromId;
--- build/types/utils/Time.d.ts
+++ build/types/utils/Time.d.ts
@@ -1,2 +1,1 @@
 export declare const getPeriodNameFromId: (periodId: string | undefined) => string;
-export declare const getPeriodISOFromId: (periodId: any) => any;
